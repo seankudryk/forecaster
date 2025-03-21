06761aad9837a4fdc2cff3343405253be101ec70
seankudryk <seankudryk@hotmail.ca>
2025-03-21 13:23:27 -0400

    feature list is the same,some changes to html, and added a stylesheet to make it look nicer

--- src/display.js
+++ src/display.js
@@ -8,8 +8,8 @@ const currentConditions = document.querySelector("#current-conditions");
 export const displayData = (dataObject) => { 
     cityName.textContent = dataObject.cityName;
     fullLocationName.textContent = dataObject.fullLocationName;
-    currentTemperature.textContent = dataObject.currentTemperature;
-    currentHigh.textContent = dataObject.currentHigh;
-    currentLow.textContent = dataObject.currentLow;
+    currentTemperature.textContent = `Current Temperatute: ${dataObject.currentTemperature}\u00B0`;
+    currentHigh.textContent = `High: ${dataObject.currentHigh}\u00B0`;
+    currentLow.textContent = `Low: ${dataObject.currentLow}\u00B0`;
     currentConditions.textContent = dataObject.currentConditions;   
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { getData } from './app-logic.js';
 import { displayData } from './display.js';
+import "./styles.css";
 
 const submitButton = document.querySelector("#submit-button");
 const searchInput = document.querySelector("#search-input");
@@ -30,11 +31,12 @@ submitButton.addEventListener("click", (event) => {
             response.days[0].tempmin, 
             response.currentConditions.conditions,
         );
-
+        searchInput.setCustomValidity("");
         displayData(storedData);
     })
     .catch(error => {
         console.error("Fetch failed", error);
+        searchInput.setCustomValidity("Invalid Search Term, Please try again");
     });
 
     //reset input value back to nothing
