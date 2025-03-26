ca7cb994733aab682b0b55543bea17b71c566c41
seankudryk <seankudryk@hotmail.ca>
2025-03-26 15:05:18 -0400

    Iconify svg render is just refusing to work for whatever reason, I've been over the documentation plenty of times and it doesnt want to work - too frustrated to keep just plugging away at this for now

--- src/display.js
+++ src/display.js
@@ -9,18 +9,28 @@ const currentConditions = document.querySelector("#current-conditions");
 const bigTemperature = document.querySelector("#big-temperature");
 
 const generateSVG = (weatherCondition) => {
-    let searchTerm;
 
-    if (weatherCondition === "Clear") {
-        searchTerm = "day-rain";
-    } 
+    let searchTerm = weatherCondition.toLowerCase();
+    console.log(searchTerm);
 
-    const svg = Iconify.renderSVG(`wi:${searchTerm}`, {
+    const weatherConditionMap = {
+        clear: "wi:day-sunny",
+        overcast: "wi:day-cloudy",
+        partiallyCloudy: "wi:day-sunny-overcast" 
+    }
+
+    console.log(weatherConditionMap.clear);
+
+    const svg = Iconify.renderSVG("wi:day-sunny", {
         height: 180,
         width: 180
     });   
     
-    document.body.appendChild(svg);
+    if (svg) {
+        document.body.appendChild(svg);
+    } else {
+        console.warn("Iconfiy failed to render SVG");
+    }
 }
 
 
