034a54687d4c84b2ba317d4f2285e08f9f4ba8a1
seankudryk <seankudryk@hotmail.ca>
2025-03-28 11:48:52 -0400

    refactored code to allow for the API call and data handling to be done outside of the submitButton click event, on page load, the web app calls handleData(toronto) to allow the user to see how the weather data will be displayed immediately

--- src/index.js
+++ src/index.js
@@ -6,77 +6,92 @@ const submitButton = document.querySelector("#submit-button");
 const searchInput = document.querySelector("#search-input");
 const degreeCheckbox = document.querySelector("#degree-checkbox");
 
+const handleData = (userInput) => {
+    if (!userInput) {
+        searchInput.setCustomValidity("Invalid Search Term, Please try again");
+        console.warn("Invalid search term");
+        return;
+    } else {
+        const requestData = getData(userInput);
+    
+        requestData.then(response => {
+            console.log(response);
+            function WeatherData(cityName, fullLocationName, currentTemperature, currentHigh, currentLow, currentConditions) {
+                this.cityName = cityName;
+                this.fullLocationName = fullLocationName;
+
+                //immutable values from API call that will be returned when degress F is selected (or, when degreeCheckbox is "checked")
+                this.originalTemperature = currentTemperature;
+                this.originalHigh = currentHigh;
+                this.originalLow = currentLow;
+
+                //mutable values to be used to convert original call in degrees Fahrenheit to degrees Celsius
+                this.currentTemperature = currentTemperature;
+                this.roundedCurrentTemperature = Math.round(currentTemperature);
+                this.currentHigh = currentHigh;
+                this.currentLow = currentLow; 
+                this.currentConditions = currentConditions;
+
+                this.toCelsius = () => {
+                    this.currentTemperature = ((currentTemperature - 32) * 5/9).toFixed(1);
+                    this.currentHigh = ((currentHigh - 32) * 5/9).toFixed(1);
+                    this.currentLow = ((currentLow - 32) * 5/9).toFixed(1);
+                    this.roundedCurrentTemperature = Math.round(this.currentTemperature);
+                }
+                
+                this.toFahrenheit = () => {
+                    this.currentTemperature = this.originalTemperature.toFixed(1);
+                    this.currentHigh = this.originalHigh.toFixed(1);
+                    this.currentLow = this.originalLow.toFixed(1);
+                    this.roundedCurrentTemperature = Math.round(this.originalTemperature);
+                }
+            };
+
+            searchInput.setCustomValidity("");
+    
+            const storedData = new WeatherData(
+                response.address, 
+                response.resolvedAddress, 
+                response.currentConditions.temp, 
+                response.days[0].tempmax, 
+                response.days[0].tempmin, 
+                response.currentConditions.conditions,
+            );
+    
+            if (degreeCheckbox.checked) {
+                displayData(storedData);
+            } else {
+                storedData.toCelsius();
+                console.log(storedData);
+                displayData(storedData);
+            }
+    
+            degreeCheckbox.addEventListener("click", () => {
+                if (degreeCheckbox.checked) {
+                    storedData.toFahrenheit();
+                    displayData(storedData);
+                } else {
+                    storedData.toCelsius();
+                    displayData(storedData);
+                }
+            });
+        })
+        .catch(error => {
+            console.error("Fetch failed", error);
+            searchInput.setCustomValidity("Invalid Search Term, Please try again");
+        });
+    };
+}
+
 submitButton.addEventListener("click", (event) => {
     //prevent default submit conditions from firing when the button is clicked so we can do our own event handling
     event.preventDefault();
-
-    //call getData, passing the current .value of the searchInput element as argumengetData(searchInput.value);
-    const requestData = getData(searchInput.value);
-
-    requestData.then(response => {
-        console.log(response);
-        function WeatherData(cityName, fullLocationName, currentTemperature, currentHigh, currentLow, currentConditions) {
-            this.cityName = cityName;
-            this.fullLocationName = fullLocationName;
-            this.currentTemperature = currentTemperature;
-            this.roundedCurrentTemperature = Math.round(currentTemperature);
-            this.currentHigh = currentHigh;
-            this.currentLow = currentLow; 
-            this.currentConditions = currentConditions;
-            this.toCelsius = () => {
-                this.currentTemperature = ((currentTemperature - 32) * 5/9).toFixed(1);
-                this.currentHigh = ((currentHigh - 32) * 5/9).toFixed(1);
-                this.currentLow = ((currentLow - 32) * 5/9).toFixed(1);
-                this.roundedCurrentTemperature = Math.round(this.currentTemperature);
-            }
-            this.toFahrenheit = () => {
-                this.currentTemperature = ((currentTemperature * 9/5) + 32).toFixed(1);
-                this.currentHigh = ((currentHigh * 9/5) + 32).toFixed(1);
-                this.currentLow = ((currentLow * 9/5) + 32).toFixed(1);
-                this.roundedCurrentTemperature = Math.round(this.currentTemperature);
-            }
-        };
-
-        const storedData = new WeatherData(
-            response.address, 
-            response.resolvedAddress, 
-            response.currentConditions.temp, 
-            response.days[0].tempmax, 
-            response.days[0].tempmin, 
-            response.currentConditions.conditions,
-        );
-
-        searchInput.setCustomValidity("");
-
-        if (degreeCheckbox.checked) {
-            displayData(storedData);
-        } else {
-            storedData.toCelsius();
-            console.log(storedData);
-            displayData(storedData);
-        }
-
-        degreeCheckbox.addEventListener("click", () => {
-            //
-            if (degreeCheckbox.checked) {
-                storedData.toCelsius();;
-                displayData(storedData);
-            } else {
-                storedData.toFahrenheit();
-                displayData(storedData);
-            }
-        });
-    })
-    .catch(error => {
-        console.error("Fetch failed", error);
-        searchInput.setCustomValidity("Invalid Search Term, Please try again");
-    });
-
-    //reset input value back to nothing
+    
+    handleData(searchInput.value);
     searchInput.value = "";
 });
 
-
+handleData("Toronto");
 
 
 
